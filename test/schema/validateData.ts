b41af6ae2ed1d1227998d613e221881141533216
Johan Pujol <johan.pujol@gmail.com>
2016-11-21 15:17:22 +0100

    Merge

--- test/schema/validateData.ts
+++ test/schema/validateData.ts
@@ -1,8 +1,7 @@
-import {TestContainer} from '../../dist/di/containers';
-import {expect} from 'chai';
-import {Model, Property, Reference, Validator} from '../../dist/schemas/annotations';
-import {Domain} from '../../dist/schemas/schema';
-import {Container} from '../../src/di/containers';
+import {TestContainer} from "../../dist/di/containers";
+import {expect} from "chai";
+import {Model, Property, Reference, Validator} from "../../dist/schemas/annotations";
+import {Domain} from "../../dist/schemas/schema";
 
 @Model()
 class BaseModel {
@@ -51,43 +50,42 @@ class DateIsoModel {
 }
 
 
-
 let container = new TestContainer("Test");
 
 describe("Validate data", function () {
 
-    it("should validate base class", async () => {
+    it("should validate base class", async() => {
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("SimpleModel");
 
-        let model: SimpleModel = { text: "text", number: 1, baseText: "" };
+        let model: SimpleModel = {text: "text", number: 1, baseText: ""};
         let errors = await schema.validateAsync(null, model);
         expect(errors.length).equals(1);
         expect(errors[0].property).equals("baseText");
     });
 
-    it("should validate call validator", async () => {
+    it("should validate call validator", async() => {
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("SimpleModel");
 
-        let model: SimpleModel = { text: "text", number: 1, baseText: "a" };
+        let model: SimpleModel = {text: "text", number: 1, baseText: "a"};
         let errors = await schema.validateAsync(null, model);
         expect(errors.length).equals(1);
         expect(errors[0].property).equals("baseText");
     });
 
-    it("should validate malformed number", async () => {
+    it("should validate malformed number", async() => {
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("SimpleModel");
 
-        let model = schema.bind({ text: "text", number: "1w1", baseText: "text" });
+        let model = schema.bind({text: "text", number: "1w1", baseText: "text"});
         let errors = await schema.validateAsync(null, model);
         expect(errors.length).equals(1);
         expect(errors[0].property).equals("number");
     });
 
-    it("should validate valid values", async () => {
-        let model: SimpleModel = { text: "text", number: 1, baseText: "text" };
+    it("should validate valid values", async() => {
+        let model: SimpleModel = {text: "text", number: 1, baseText: "text"};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("SimpleModel");
         let errors = await schema.validateAsync(null, model);
@@ -98,21 +96,21 @@ describe("Validate data", function () {
 
     // ---------------
     // email
-    it('should validate email value', () => {
+    it('should validate email value', async() => {
 
         let model: EmailModel = {email: "first.name@email.com"};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("EmailModel");
-        let errors = schema.validate(model);
+        let errors = await schema.validateAsync(null, model);
 
         expect(errors.length).equals(0, 'The email is malformed');
     });
-    it('should validate malformed email value', () => {
+    it('should validate malformed email value', async() => {
 
         let model: EmailModel = {email: "first.name@email"};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("EmailModel");
-        let errors = schema.validate(model);
+        let errors = await schema.validateAsync(null, model);
 
         expect(errors.length).equals(1);
     });
@@ -120,63 +118,65 @@ describe("Validate data", function () {
 
     // ---------------
     // url
-    it('should validate url value', () => {
+    it('should validate url value', async() => {
 
         let model: UrlModel = {url: "https://myWebsite.com/#ancre/1"};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("UrlModel");
-        let errors = schema.validate(model);
+        let errors = await schema.validateAsync(null, model);
 
         expect(errors.length).equals(0);
     });
-    it('should validate malformed url value', () => {
+    it('should validate malformed url value', async() => {
 
         let model: UrlModel = {url: "http://site.r"};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("UrlModel");
-        let errors = schema.validate(model);
+        let errors = await schema.validateAsync(null, model);
 
         expect(errors.length).equals(1);
     });
 
     // ---------------
     // Alphanumeric
-    it('should validate alphanumeric value', () => {
+    it('should validate alphanumeric value', async() => {
 
         let model: AlphanumericModel = {value: "abcde1345fghik6789"};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("AlphanumericModel");
-        let errors = schema.validate(model);
+        let errors = await schema.validateAsync(undefined, model);
 
         expect(errors.length).equals(0);
     });
-    it('should validate malformed alphanumeric value', () => {
+    it('should validate malformed alphanumeric value', async() => {
 
         let model: AlphanumericModel = {value: "abc123!"};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("AlphanumericModel");
-        let errors = schema.validate(model);
+        let errors = await schema.validateAsync(undefined, model);
 
         expect(errors.length).equals(1);
     });
 
     // ---------------
     // Date ISO86
-    it('should validate date ISO8601', () => {
+    it('should validate date ISO8601', async() => {
 
         let model: DateIsoModel = {date: new Date().toISOString()};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("DateIsoModel");
-        let errors = schema.validate(model);
+        let errors = await schema.validateAsync(undefined, model);
 
         expect(errors.length).equals(0);
     });
-    it('should validate malformed date ISO8601', () => {
+
+
+    it('should validate malformed date ISO8601', async() => {
 
         let model: DateIsoModel = {date: new Date().toDateString()};
         let domain = container.get<Domain>("Domain");
         let schema = domain.getSchema("DateIsoModel");
-        let errors = schema.validate(model);
+        let errors = await schema.validateAsync(undefined, model);
 
         expect(errors.length).equals(1);
     });
